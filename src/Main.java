a0cfa1981c7344f19edc8fb43b4f6c1aa90f7ab6
Andrew Sylvester <andrewrsylvester@gmail.com>
2019-11-11 20:04:21 -0600

    Most of quicksorter set up. Debugging needed

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,31 @@
+import java.util.ArrayList;
 
 public class Main 
 {
 	public static void main(String[] args) 
 	{
+		ArrayList<Integer> initialList = QuickSorter.generateRandomList(30);
+		System.out.println(initialList.toString());
 		
+		ArrayList<Integer> firstList = new ArrayList<Integer>();
+		firstList.addAll(initialList);
+		System.out.println(QuickSorter.timedQuickSort(firstList, QuickSorter.PivotStrategy.FIRST_ELEMENT));
+		System.out.println(firstList.toString());
+		
+		ArrayList<Integer> randomList = new ArrayList<Integer>();
+		randomList.addAll(initialList);
+		System.out.println(QuickSorter.timedQuickSort(randomList, QuickSorter.PivotStrategy.RANDOM_ELEMENT));
+		System.out.println(randomList.toString());
+		
+		ArrayList<Integer> randomMedianList = new ArrayList<Integer>();
+		randomMedianList.addAll(initialList);
+		System.out.println(QuickSorter.timedQuickSort(randomMedianList, QuickSorter.PivotStrategy.MEDIAN_OF_THREE_RANDOM_ELEMENTS));
+		System.out.println(randomMedianList.toString());
+		
+		ArrayList<Integer> medianList = new ArrayList<Integer>();
+		medianList.addAll(initialList);
+		System.out.println(QuickSorter.timedQuickSort(medianList, QuickSorter.PivotStrategy.MEDIAN_OF_THREE_ELEMENTS));
+		System.out.println(medianList.toString());
 	}
 
 }
--- src/QuickSorter.java
+++ src/QuickSorter.java
@@ -1,9 +1,11 @@
 
 import java.time.Duration;
 import java.util.ArrayList;
+import java.util.Random;
 
 public class QuickSorter
 {
+	private static final int SMALL_ARRAY_LIMIT = 20;
 
     /*
      * This private constructor is optional, but it does help to prevent accidental client instantiation of QuickSorter
@@ -12,16 +14,222 @@ public class QuickSorter
      */
     private QuickSorter() { }
 
-    public static <E extends Comparable<E>> Duration timedQuickSort(ArrayList<E> list, PivotStrategy strategy)
+    public static <E extends Comparable<E>> Duration timedQuickSort(ArrayList<E> list, PivotStrategy strategy) throws NullPointerException
     {
-		return null;
-        // TODO implement timedQuickSort(ArrayList<E>, PivotStrategy)
+    	if(list == null || strategy == null)
+    	{
+    		throw new NullPointerException("Argument[s] are null!");
+    	}
+    	
+    	long startTime = 0;
+    	
+    	switch(strategy)
+    	{
+    	case FIRST_ELEMENT:
+    		startTime = System.nanoTime();
+    		firstElementQuickSort(list, 0, list.size() - 1);
+    		break;
+    	case RANDOM_ELEMENT:
+    		startTime = System.nanoTime();
+    		randomElementQuickSort(list, 0, list.size() - 1);
+    		break;
+    	case MEDIAN_OF_THREE_RANDOM_ELEMENTS:
+    		startTime = System.nanoTime();
+    		randomMedianQuickSort(list, 0, list.size() - 1);
+    		break;
+    	case MEDIAN_OF_THREE_ELEMENTS:
+    		startTime = System.nanoTime();
+    		medianQuickSort(list, 0, list.size() - 1);
+    		break;
+    	}
+    	
+    	long finishTime = System.nanoTime();
+        Duration elapsedTime = Duration.ofNanos(finishTime - startTime);
+		return elapsedTime;
+    }
+    
+    private static <E extends Comparable<E>> void firstElementQuickSort(ArrayList<E> list, int left, int right)
+    {
+    	//If subarray is smaller than 20 then use insertion sort.
+    	if(left + SMALL_ARRAY_LIMIT <= right)
+    	{
+    		 // Place pivot at position right - 1
+            swapElements( list, left, right);
+    		E pivot = list.get(right);
+    		
+    		int i = left;
+    		int j = right - 1;
+    		while(true)
+    		{
+    			while(list.get(++i).compareTo(pivot) < 0) {} //Just iterate till found greater element.
+    			while(list.get(--j).compareTo(pivot) > 0) {} //Just iterate till found lesser element.
+    			if( i < j)
+    				swapElements(list, i, j);
+    			else
+    				break;
+    		}
+    			swapElements(list, i, right - 1 );   // Restore pivot
+    			
+    			firstElementQuickSort(list, left, i - 1);
+    			firstElementQuickSort(list, i + 1, right);
+    	}
+    	else
+    		insertionSort(list, left, right);
+    }
+    
+    private static <E extends Comparable<E>> void randomElementQuickSort(ArrayList<E> list, int left, int right)
+    {
+    	//If subarray is smaller than 20 then use insertion sort.
+    	if(left + SMALL_ARRAY_LIMIT <= right)
+    	{
+    		Random random = new Random();
+    		// Place pivot at position right - 1
+            swapElements( list, random.nextInt(right - left + 1) + left, right - 1 );
+    		E pivot = list.get(right - 1);
+    		
+    		int i = left;
+    		int j = right - 1;
+    		while(true)
+    		{
+    			while(list.get(++i).compareTo(pivot) < 0) {} //Just iterate till found greater element.
+    			while(list.get(--j).compareTo(pivot) > 0) {} //Just iterate till found lesser element.
+    			if( i < j)
+    				swapElements(list, i, j);
+    			else
+    				break;
+    		}
+    			swapElements(list, i, right - 1 );   // Restore pivot
+    			
+    			firstElementQuickSort(list, left, i - 1);
+    			firstElementQuickSort(list, i + 1, right);
+    	}
+    	else
+    		insertionSort(list, left, right);
+    }
+    
+    private static <E extends Comparable<E>> void randomMedianQuickSort(ArrayList<E> list, int left, int right)
+    {
+    	//If subarray is smaller than 20 then use insertion sort.
+    	if(left + SMALL_ARRAY_LIMIT <= right)
+    	{
+    		E pivot = randomMedian(list, left, right);
+    		
+    		int i = left;
+    		int j = right - 1;
+    		while(true)
+    		{
+    			while(list.get(++i).compareTo(pivot) < 0) {} //Just iterate till found greater element.
+    			while(list.get(--j).compareTo(pivot) > 0) {} //Just iterate till found lesser element.
+    			if( i < j)
+    				swapElements(list, i, j);
+    			else
+    				break;
+    		}
+    			swapElements(list, i, right - 1 );   // Restore pivot
+    			
+    			firstElementQuickSort(list, left, i - 1);
+    			firstElementQuickSort(list, i + 1, right);
+    	}
+    	else
+    		insertionSort(list, left, right);
+    }
+    
+    private static <E extends Comparable<E>> E randomMedian(ArrayList<E> list, int left, int right)
+    {
+    	Random random = new Random();
+    	int x = random.nextInt(right - left + 1) + left;
+    	int y = random.nextInt(right - left + 1) + left;
+    	int z = random.nextInt(right - left + 1) + left;
+    	
+        if(list.get(y).compareTo(list.get(x) ) < 0 )
+            swapElements( list, x, y );
+        if( list.get(z).compareTo(list.get(x) ) < 0 )
+        	swapElements( list, x, z );
+        if( list.get(z).compareTo(list.get(y) ) < 0 )
+        	swapElements( list, y, z );
+
+        // Place pivot at position right - 1
+        swapElements( list, y, right - 1 );
+        return list.get(right - 1);
+    }
+    
+    private static <E extends Comparable<E>> void medianQuickSort(ArrayList<E> list, int left, int right)
+    {
+    	//If subarray is smaller than 20 then use insertion sort.
+    	if(left + SMALL_ARRAY_LIMIT <= right)
+    	{
+    		E pivot = median(list, left, right);
+    		
+    		int i = left;
+    		int j = right - 1;
+    		while(true)
+    		{
+    			while(list.get(++i).compareTo(pivot) < 0) {} //Just iterate till found greater element.
+    			while(list.get(--j).compareTo(pivot) > 0) {} //Just iterate till found lesser element.
+    			if( i < j)
+    				swapElements(list, i, j);
+    			else
+    				break;
+    		}
+    			swapElements(list, i, right - 1 );   // Restore pivot
+    			
+    			firstElementQuickSort(list, left, i - 1);
+    			firstElementQuickSort(list, i + 1, right);
+    	}
+    	else
+    		insertionSort(list, left, right);
+    }
+    
+    private static <E extends Comparable<E>> E median(ArrayList<E> list, int left, int right)
+    {
+    	int center = ( left + right ) / 2;
+    	if(list.get(center).compareTo(list.get(left) ) < 0 )
+            swapElements( list, left, center );
+        if( list.get(right).compareTo(list.get(left) ) < 0 )
+        	swapElements( list, left, right );
+        if( list.get(right).compareTo(list.get(center) ) < 0 )
+        	swapElements( list, center, right );
+
+        // Place pivot at position right - 1
+        swapElements( list, center, right - 1 );
+        return list.get(right - 1);
+    }
+    
+    private static <E extends Comparable<E>> void swapElements(ArrayList<E> list, int i, int j)
+    {
+    	E temp = list.get(i);
+    	list.set(i, list.get(j));
+    	list.set(j, temp);
+    }
+    
+    private static <E extends Comparable<E>> void insertionSort(ArrayList<E> list, int left, int right)
+    {
+    	for(int x = left + 1; x <= right; x++)
+    	{
+    		E current = list.get(x);
+    		int y;
+    		for(y = x; y > left && current.compareTo(list.get(y - 1)) < 0; y--)
+    		{
+    			list.set(y, list.get(y - 1));
+    		}
+    		list.set(y, current);
+    	}
     }
 
-    public static ArrayList<Integer> generateRandomList(int size)
+    public static ArrayList<Integer> generateRandomList(int size) throws IllegalArgumentException
     {
-		return null;
-        // TODO implement generateRandomList(int)
+    	if(size < 0)
+    		throw new IllegalArgumentException("Size should be nonnegative!");
+    	
+    	Random random = new Random();
+    	ArrayList<Integer> list = new ArrayList<Integer>();
+    	
+    	for(int x = 0; x < size; x++)
+    	{
+    		list.add(random.nextInt(50 - -50 + 1) + -50);
+    	}
+    	
+		return list;
     }
 
     public static enum PivotStrategy
